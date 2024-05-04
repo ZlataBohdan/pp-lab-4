72d2ecd4a17e646edb88db1802669f2849e4aba0
ZlataBohdan <septvanlove@gmail.com>
2024-05-04 15:34:23 +0200

    Added code for task 3

--- employees/Company.java
+++ employees/Company.java
@@ -1,15 +1,24 @@
-public class Company {
-    public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
-        employees[0] = new Manager("John Doe", 50000);
-        employees[1] = new Worker("Jane Smith", 60000, "Developer");
-        employees[2] = new Worker("Michael Johnson", 55000, "QA Engineer");
-        employees[3] = new Manager("Emily Davis", 52000);
-        employees[4] = new Worker("David Brown", 53000, "Designer");
+public static void main(String[] args) {
+    Employee[] employees = new Employee[5];
+    employees[0] = new Manager("John Doe", 50000);
+    employees[1] = new Worker("Jane Smith", 60000, "Developer");
+    employees[2] = new Worker("Michael Johnson", 55000, "QA Engineer");
+    employees[3] = new Manager("Emily Davis", 52000);
+    employees[4] = new Worker("David Brown", 53000, "Designer");
 
-        System.out.println("Details for all employees:");
-        for (Employee emp : employees) {
-            System.out.println(emp);
+    int nonManagerCount = 0;
+
+    for (Employee emp : employees) {
+        if (!(emp instanceof Manager)) {
+            nonManagerCount++;
         }
+    }
+
+    employees[0].setNumberOfSubordinates(nonManagerCount);
+    employees[0].setSalary(7500);
+
+    System.out.println("Details for all employees:");
+    for (Employee emp : employees) {
+        System.out.println(emp);
     }
 }
